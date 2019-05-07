7caa45e3d28c034ecc9254e5bce594347a994470
Vanessamf <78355655cc@sina.cn>
2019-05-07 17:35:45 +0800

    Update main.comm.js

--- javascript/main.comm.js
+++ javascript/main.comm.js
@@ -701,8 +701,8 @@ function($) {
 
 
 $(document).ready(function() {
-	$("#menu_header").load("/商户自助平台new2/common/header.html");
-	$("#nav").load("/商户自助平台new2/common/nav.html");
+	$("#menu_header").load("-new2/common/header.html");
+	$("#nav").load("-new2/common/nav.html");
 });
 
 function quanxuan(a, b) {
